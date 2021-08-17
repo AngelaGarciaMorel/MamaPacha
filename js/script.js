4ab54715bbe4c1ade07f194b20df6eb5f5491431
Mariano Jimenez Perez <marianojimenezperez1@gmail.com>
2021-08-17 19:12:12 -0300

    se agregan animaciones

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const cargaWeb = document.querySelector('.carga_web')
 const contenido = document.querySelector('#contenido')
 const body = document.querySelector('body')
+let objetoAnimado = document.querySelectorAll('.animado')
 
 setTimeout(inicio, 4000);
 
@@ -9,3 +10,16 @@ function inicio(){
     contenido.classList.remove('d-none')
     body.classList.add('bkg-imagen')
 }
+
+function scrollAlto(){
+    let scrollTop = document.documentElement.scrollTop;
+    for ( let i = 0; i < objetoAnimado.length; i++){
+        let alturaObjeto = objetoAnimado[i].offsetTop;
+        console.log(scrollTop)
+        if(alturaObjeto - 500 < scrollTop){
+            objetoAnimado[i].style.opacity = 1;
+            objetoAnimado[i].classList.add('hacia-arriba')
+        }
+    }
+}
+window.addEventListener('scroll', scrollAlto);
